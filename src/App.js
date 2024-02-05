1988ab40d78963cd0e90c99db19e086fca37a640
Akasharyan47 <akasharyan.aryan@gamil.com>
2024-02-05 20:26:44 +0530

    ak

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ const App = () => {
   return (
     <ThemeProvider theme={theme}>
        <GlobalStyle />
-       <BrowserRouter>
+       <BrowserRouter> 
         <Header />
         <Routes> 
          <Route path="/" element={<Home />} />
